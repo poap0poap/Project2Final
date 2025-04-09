b671828b70418f9ac938ee00d9533717e95dc814
Anonymous-Yee <vila3401@colorado.edu>
2025-04-09 15:47:21 -0600

    Yes

--- Board.h
+++ Board.h
@@ -17,11 +17,13 @@ private:
 public:
     Board();
     Board(int player_count);
+    int getBoardSize() const; // Declaration of the getter method
     void displayTrack(int player_index);
-    void initializeBoard();
+    void initializeBoard(int player_count);
     void displayBoard();
-    bool movePlayer(int player_index);
+    bool movePlayer(int player_index, int spaces);
     int getPlayerPosition(int player_index) const;
+    char getTileIndex(int player_index, int pos);
 };
 
 #endif
