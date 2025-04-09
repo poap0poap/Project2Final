5dd93a3989ed8d7189489b7914558cf9adaaeee1
poap0poap <30390818+poap0poap@users.noreply.github.com>
2025-04-09 03:05:40 -0600

    works

--- Main.cpp
+++ Main.cpp
@@ -11,7 +11,6 @@
 #include <thread>
 #include <atomic>
 #include "Board.h"
-#include "GameExit.h"
 
 using namespace std;
 
@@ -22,7 +21,7 @@ int diceRoll(int dice_size){
 };
 
 //check for what tile you are on and applys effects
-string tileCheck(Board& game, int player_position, int player_path)
+string tileAction(Board& game, int player_position, int player_path)
 {
     char p = game.getTileIndex(player_path,player_position);// find landed tile using chosen path and current position
     //describes events based on landed tile
@@ -66,71 +65,129 @@ string tileCheck(Board& game, int player_position, int player_path)
     {
         return "Exceeded Board Size Ending";
     }
-};
+}
+
+//menu display
+void menuDisplay(){
+    cout << endl;
+    cout << "Press space to roll for turn" << endl;
+    cout << "Press 1 for Stats" << endl;
+    cout << "Press 2 for current Charcter" << endl;
+    cout << "Press 3 for current Boardstate" << endl;
+    cout << "Press 4 for current Advisor" << endl;
+    cout << endl;
+}
+
+//stat display
+void displayStats(Board& game,int player){
+    cout << "Stats are work in progress" << endl;
+}
+
+//charcter inforemation
+void charInfo(Board& game,int player){
+    cout << "Charcters are a work in progress" << endl;
+}
+
+//display board
+void displayBoard(Board& game,int player){
+    cout << "current boardstate:"<< endl;
+    game.displayBoard();
+}
+
+//advisor information
+void displayAdvisor(Board& game,int player){
+    cout << "Advisor is a work in progress" << endl;
+}
+
+//rolling turn actions
+int turn(Board& game,int current_player,int board, int* player){
+    char keypress = _getch();
+    if (keypress == ' '){
+        int move = diceRoll(6);//dice size is 6 may change later
+        game.movePlayer(current_player, move);
+        //cout << "Player " << current_player + 1 << " path: ";
+        game.displayTrack(current_player);
+        //move player current_playerased on dice
+        player[current_player] += move;
+        cout << tileAction(game, player[current_player], current_player) << endl;
+
+        //if pos is farther then board stop
+        if (game.getPlayerPosition(current_player) >= board){
+            cout << "Game over\n";
+            return -1;
+        }
+        return 0;
+    }
+    else{
+        switch (keypress){
+            case '1':
+                displayStats(game, current_player);
+                break;
+            case '2':
+                charInfo(game, current_player);
+                break;
+            case '3':
+                displayBoard(game, current_player);
+                break;
+            case '4':
+                displayAdvisor(game, current_player);
+                break;
+            case '5':
+                cout << "just press space" << endl;
+                break;
+            case 27: //if esc press stop
+                return -1;
+            default:
+                cout << "issues" << endl;
+                break;
+        }
+    }
+    cout << endl;
+    return 1;
+}
+
 
 int main() {
-    //multi threading
-    //allows game to always check for esc
-    atomic<bool> running(true);
-    thread quitThread(checkExit, ref(running));
+    bool running = true;
 
-    srand(time(0)); // Seed RNG once
+    srand(time(0)); //Seed RNG once
 
     //data/variables
     int players = 1;
-    int player[2] = {0}; // initlize player positions
+    int player[2] = {0}; //initlize player positions
     Board game(players);
     int board = game.getBoardSize();
 
-    game.displayBoard(); // display board
+    game.displayBoard(); //display board
 
     while (running){
         for (int i = 0; i < board; i++) {//loop for each tile - if get unlucky will reach end
             for (int b = 0; b < players + 1; b++) {//loop for each player - changes between 0 & 1 for each path/player
-                cout << "Player " << b + 1 << " path: ";
-                cout << "Press space to roll" << endl;
                 //only rolling if space pressed
+                menuDisplay();
                 while (running){
-                    char keypress = _getch();
-                    if (keypress == ' '){
-                        int move = diceRoll(6);//dice size is 6 may change later
-                        game.movePlayer(b, move);
-                        game.displayTrack(b);
-                        //move player based on dice
-                        player[b] += move;
-                        cout << tileCheck(game, player[b], b) << endl;
-
-                        // if pos is farther then board stop
-                        if (game.getPlayerPosition(b) >= board){
-                            cout << "Game over\n";
-                            running = false;
-                            break;
-                        }
+                    int run = turn(game,b,board,player);
+                    if (run == -1){
+                        running = false;
                         break;
                     }
-                    //if esc press stop
-                    if (keypress == 27){
-                        running = false; 
+                    else if (run == 0){
                         break;
                     }
+                    else if (run == 1){}
                 }
             }
-            if (!running) break;
-
-            // make space
-            for (int i = 0; i < 3; i++) {
+            if (rand() % 2 == 0){
                 cout << endl;
+                cout << "random event occurs" << endl;
             }
-            game.displayBoard();
+            if (!running) break;
         }
 
         
     }
 
-    running = false; // tell all threads to stop
-    quitThread.join(); // wait for all threads to stop
     cout << "Game terminated.\n";
-
     cout << "Press any key to exit";
     _getch();
     return 0;
